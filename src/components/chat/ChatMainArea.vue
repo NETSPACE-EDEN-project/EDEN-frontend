ef7f06443cb4584913caeb87d2e8247ad538d09e
Rean Tsai <aobc55625@gmail.com>
2025-09-17 01:27:33 +0800

    feat: implement ChatHeader with room info display and integrate sticky header layout

--- src/components/chat/ChatMainArea.vue
+++ src/components/chat/ChatMainArea.vue
@@ -3,6 +3,7 @@ import { computed } from 'vue'
 import { useChat } from '../../composables/useChat.js'
 
 import ChatEmptyState from './ChatEmptyState.vue'
+import ChatHeader from './ChatHeader.vue'
 
 const props = defineProps({
   isMobile: {
@@ -23,9 +24,20 @@ const hasCurrentRoom = computed(() => !!currentRoom.value)
 </script>
 
 <template>
-  <div class="flex flex-col flex-1 p-4">
+  <div class="flex flex-col flex-1">
+    <!-- 頭部固定 -->
+    <div class="sticky top-0 z-10 p-4 bg-white/50">
+      <ChatHeader
+        :has-current-room="hasCurrentRoom"
+        :is-mobile="isMobile"
+        @toggle-sidebar="emit('toggle-sidebar')"
+      />
+    </div>
+
     <!-- 主要內容區域 -->
-    <div v-if="hasCurrentRoom" class="flex flex-col flex-1"></div>
+    <div v-if="hasCurrentRoom" class="flex flex-col flex-1 p-4">
+      <!-- 放 MessageList / MessageInput -->
+    </div>
 
     <!-- 空狀態 -->
     <ChatEmptyState v-else />
